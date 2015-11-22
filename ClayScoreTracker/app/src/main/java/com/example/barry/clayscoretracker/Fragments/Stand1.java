54ee4464431eef810d3ae490cdffc494d32ce893
barrypearseburke <barry.burke3@mydit.ie>
2015-11-22 01:33:05 +0000

    basiclistworking

--- ClayScoreTracker/app/src/main/java/com/example/barry/clayscoretracker/Fragments/Stand1.java
+++ ClayScoreTracker/app/src/main/java/com/example/barry/clayscoretracker/Fragments/Stand1.java
@@ -13,13 +13,27 @@ import android.widget.ListView;
 import com.example.barry.clayscoretracker.R;
 
 public class Stand1 extends Fragment {
+    ListView mList;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-
-        return inflater.inflate(R.layout.stand1,container,false);
-
+        View root = inflater.inflate(R.layout.stand1,container,false);
+        mList = (ListView) root.findViewById(R.id.Stand1list);
+        return root;
     }
-}
+
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+        populateListView();
+    }
+
+    private void populateListView() {
+        String[] pair = {"Pair1","Pair2","Pair3","Pair4","Pair5"};
+
+        //build adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.row_layout,pair);
+
+        mList.setAdapter(adapter);
+    }}
